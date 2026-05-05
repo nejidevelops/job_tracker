68274758d10c9fdfcabd534b99f7024813466d46
Bofulo <newtonombese1@gmail.com>
2026-05-05 09:57:58 +0300

    Working on the updates of the job model controllers and views

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,7 @@
 class Job < ApplicationRecord
     validates :title, presence: true
     validates :company, presence: true
+    validates :status, presence: true, inclusion: { in: statuses.keys }
 
     enum :status, { applied: "applied", interviewing: "interviewing", rejected: "rejected", offered: "offered", hired: "hired" }
 end
