afa7bb4790b1ea995d8a3246c8c622c2a1f1bff2
Bofulo <newtonombese1@gmail.com>
2026-05-06 01:39:06 +0300

    Working on the updates of the delete and implementing it on the views files

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -20,6 +20,21 @@ class JobsController < ApplicationController
     end
   end
 
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to @job
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+  end
+
   private
 
   def job_params
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,8 @@
 class Job < ApplicationRecord
+    enum :status, { applied: "applied", interviewing: "interviewing", rejected: "rejected", offered: "offered", hired: "hired" }
+
     validates :title, presence: true
     validates :company, presence: true
     validates :status, presence: true, inclusion: { in: statuses.keys }
 
-    enum :status, { applied: "applied", interviewing: "interviewing", rejected: "rejected", offered: "offered", hired: "hired" }
 end
