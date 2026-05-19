ae7ef5b0e10c045e53f417beb1debfeb2821535b
Bofulo <newtonombese1@gmail.com>
2026-05-19 22:30:46 +0300

    Working on setting up the frontend on rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const container = document.getElementById('root');
+  if (container) {
+    const root = createRoot(container);
+    root.render(
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+    );
+  }
+});
